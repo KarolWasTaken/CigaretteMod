7e907dff1e9351df4f72be67b5123fdc45bf109c
Karol <Karol@cockandball.es>
2024-08-13 14:33:52 +0100

    refactored code for CigaretteBase.java to make it more readable

--- src/main/java/net/karoll/cigarettemod/Items/cigarettes/CigaretteBase.java
+++ src/main/java/net/karoll/cigarettemod/Items/cigarettes/CigaretteBase.java
@@ -3,7 +3,6 @@ package net.karoll.cigarettemod.Items.cigarettes;
 import java.util.HashMap;
 import java.util.Map;
 
-import mods.railcraft.client.particles.EntitySteamFX;
 import net.karoll.cigarettemod.CigaretteMod;
 import net.karoll.cigarettemod.ItemRegistration.ModItems;
 import net.karoll.cigarettemod.ItemRegistration.ModPotions;
@@ -22,6 +21,7 @@ import net.minecraft.world.World;
 import gregtech.api.enums.ItemList;
 import gregtech.api.enums.SoundResource;
 import gregtech.api.util.GT_Utility;
+import mods.railcraft.client.particles.EntitySteamFX;
 
 public class CigaretteBase extends Item {
 
@@ -42,32 +42,38 @@ public class CigaretteBase extends Item {
         // isLit
         // lastUseTime
 
-        SetUpCigarette(tier);
+        this._coolStatusEffectDuration = getCoolStatusEffectDuration(tier);
+        this._cigaretteButt = getCigaretteButt(tier);
         this.setMaxStackSize(1);
     }
 
-    private void SetUpCigarette(ItemTiers.Tier tier) {
+    private int getCoolStatusEffectDuration(ItemTiers.Tier tier) {
         switch (tier) {
             case crude:
-                this.setMaxDamage(1);
-                _coolStatusEffectDuration = 60 * TICK;
-                _cigaretteButt = ModItems.crudeCigaretteButt;
-                break;
+                return 60 * TICK;
             case basic:
-                this.setMaxDamage(3);
-                _coolStatusEffectDuration = 180 * TICK;
-                _cigaretteButt = ModItems.basicCigaretteButt;
-                break;
+                return 180 * TICK;
             case mediocre:
-                this.setMaxDamage(5);
-                _coolStatusEffectDuration = 300 * TICK;
-                _cigaretteButt = ModItems.mediocreCigaretteButt;
-                break;
+                return 300 * TICK;
             case finest:
-                this.setMaxDamage(7);
-                _coolStatusEffectDuration = 420 * TICK;
-                _cigaretteButt = ModItems.finestCigaretteButt;
-                break;
+                return 420 * TICK;
+            default:
+                return 0;
+        }
+    }
+
+    private CigaretteButtBase getCigaretteButt(ItemTiers.Tier tier) {
+        switch (tier) {
+            case crude:
+                return ModItems.crudeCigaretteButt;
+            case basic:
+                return ModItems.basicCigaretteButt;
+            case mediocre:
+                return ModItems.mediocreCigaretteButt;
+            case finest:
+                return ModItems.finestCigaretteButt;
+            default:
+                return null;
         }
     }
 
@@ -131,129 +137,135 @@ public class CigaretteBase extends Item {
     // please simplify this at some point man
     @Override
     public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer player) {
-        if (!worldIn.isRemote) {
-            boolean hasLighterUsed = false;
-            boolean hasLighterFull = false;
-            boolean isPlatinum = false;
-            int lighterIndex = -1;
-            ItemStack[] playerInventory = player.inventory.mainInventory;
+        if (worldIn.isRemote) return super.onItemRightClick(itemStackIn, worldIn, player);
 
-            // look for lighter and see if its already open
-            for (int i = 0; i < playerInventory.length - 1; i++) {
-                if (playerInventory[i] != null && playerInventory[i].getUnlocalizedName()
-                    .contains("gt.metaitem.01")) {
-                    String Name = playerInventory[i].getDisplayName();
-                    if (Name.contains("Lighter (Full)")) {
-                        hasLighterFull = true;
-                        lighterIndex = i;
-                        break;
-                    } else if (Name.contains("Lighter") && !Name.contains("(Empty)")) {
-                        hasLighterUsed = true;
-                        lighterIndex = i;
-                        break;
-                    }
-                }
-            }
-            // if no lighter or lighter is empty
-            if (!hasLighterUsed && !hasLighterFull) {
-                return super.onItemRightClick(itemStackIn, worldIn, player);
-            }
-            // see if item is platinum or not
-            if (playerInventory[lighterIndex].getDisplayName()
-                .contains("Platinum")) {
-                isPlatinum = true;
-            }
-            // depending on plat or not, set lighterStates
-            if (!isPlatinum) {
-                lighterStates.put("empty", ItemList.Tool_Lighter_Invar_Empty.get(1L));
-                lighterStates.put("used", ItemList.Tool_Lighter_Invar_Used.get(1L));
-                lighterStates.put("full", ItemList.Tool_Lighter_Invar_Full.get(1L));
-            } else {
-                lighterStates.put("empty", ItemList.Tool_Lighter_Platinum_Empty.get(1L));
-                lighterStates.put("used", ItemList.Tool_Lighter_Platinum_Used.get(1L));
-                lighterStates.put("full", ItemList.Tool_Lighter_Platinum_Full.get(1L));
-            }
+        // find lighter
+        int lighterIndex = findLighter(player);
+        if (lighterIndex == -1) return super.onItemRightClick(itemStackIn, worldIn, player);
 
-            // grabs lighter item and important info
-            long fuelAmount;
-            long newFuelAmount;
-            ItemStack lighterItemStack = playerInventory[lighterIndex];
-            if (hasLighterFull) {
-                if (isPlatinum) {
-                    newFuelAmount = 999L;
-                } else {
-                    newFuelAmount = 99L;
-                }
-            } else {
-                fuelAmount = GT_Utility.ItemNBT.getLighterFuel(lighterItemStack);
-                newFuelAmount = fuelAmount - 1;
-            }
-            // plays fire ignite sound at player feet
-            GT_Utility.sendSoundToPlayers(
-                worldIn,
-                SoundResource.FIRE_IGNITE,
-                1.0F,
-                1.0F,
-                (int) player.posX,
-                (int) player.posY,
-                (int) player.posZ);
-            // use fuel on lighter
-            GT_Utility.ItemNBT.setLighterFuel(lighterItemStack, newFuelAmount);
-            // see if the lighter needs a swap of texture
-            if (newFuelAmount <= 0) {
-                useUp(lighterItemStack);
-            } else {
-                prepare(lighterItemStack, newFuelAmount);
-            }
+        // see if it's platinum
+        boolean isPlatinum = isLighterPlatinum(player.inventory.mainInventory[lighterIndex]);
+        updateLighterStates(isPlatinum);
 
-            // actual cig stuff here
-            // grab the NBT for the cig to make it lit
-            NBTTagCompound tagCompound = itemStackIn.getTagCompound();
-            if (tagCompound == null) {
-                tagCompound = new NBTTagCompound();
-            }
-            if (!tagCompound.getBoolean("lit")) {
-                tagCompound.setBoolean("lit", true);
-                itemStackIn.setTagCompound(tagCompound);
-            }
-            // store when last it was lit (unlit after 10 seconds)
-            tagCompound.setLong("lastUseTime", worldIn.getTotalWorldTime());
-            itemStackIn.setTagCompound(tagCompound);
+        // decrease lighter fuel
+        ItemStack lighterItemStack = player.inventory.mainInventory[lighterIndex];
+        handleLighterFuel(lighterItemStack, isPlatinum);
 
-            // spawn smoke infront of player's face
-            Map<String, Double> smokePos = getPositionInfrontofPlayerFace(player);
-            Map<String, Double> smokeOffsets = getOffsetInfrontofPlayerFace(player);
-            // Spawn smoke particles
-            for (int i = 0; i < 5; i++) { // Number of smoke particles
-                // worldIn.spawnParticle("smoke",
-                // smokePos.get("x"),
-                // smokePos.get("y"),
-                // smokePos.get("z"),
-                // 0.0D,
-                // 0.0D,
-                // 0.0D);
-                Minecraft.getMinecraft().effectRenderer.addEffect(
-                    new EntitySteamFX(worldIn, smokePos.get("x"), smokePos.get("y"), smokePos.get("z"), smokeOffsets.get("x") / 10, smokeOffsets.get("y") / 10, smokeOffsets.get("z") / 10));
-            }
+        // set NBT as lit
+        setCigaretteLit(itemStackIn, worldIn);
+        spawnSmokeParticles(player, worldIn);
+        applyCoolStatusEffect(player);
 
-            // add cool status effect for
-            player.removePotionEffect(ModPotions.PotionIDConverter.get("coolPotion"));
-            player.addPotionEffect(
-                new PotionEffect(ModPotions.PotionIDConverter.get("coolPotion"), _coolStatusEffectDuration, 0));
-            itemStackIn.damageItem(1, player);
-            if (itemStackIn.getItemDamage() == itemStackIn.getMaxDamage()) {
-
-                ItemStack newStack = new ItemStack(_cigaretteButt, 1);
-                player.inventory.setInventorySlotContents(player.inventory.currentItem, newStack);
-                player.inventory.markDirty(); // marks inven for an update
-                // player.setCurrentItemOrArmor(0, newStack); // Replace the current item with the updated one
-                return newStack;
-            }
-        }
-        return super.onItemRightClick(itemStackIn, worldIn, player);
+        itemStackIn.damageItem(1, player);
+        return handleCigaretteDamage(itemStackIn, player);
     }
 
+    private int findLighter(EntityPlayer player) {
+        ItemStack[] inventory = player.inventory.mainInventory;
+        for (int i = 0; i < inventory.length - 1; i++) {
+            if (inventory[i] != null && inventory[i].getUnlocalizedName()
+                .contains("gt.metaitem.01")) {
+                String name = inventory[i].getDisplayName();
+                if (name.contains("Lighter")) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
 
+    private boolean isLighterPlatinum(ItemStack lighter) {
+        return lighter.getDisplayName()
+            .contains("Platinum");
+    }
+
+    private void updateLighterStates(boolean isPlatinum) {
+        lighterStates.clear();
+        if (isPlatinum) {
+            lighterStates.put("empty", ItemList.Tool_Lighter_Platinum_Empty.get(1L));
+            lighterStates.put("used", ItemList.Tool_Lighter_Platinum_Used.get(1L));
+            lighterStates.put("full", ItemList.Tool_Lighter_Platinum_Full.get(1L));
+        } else {
+            lighterStates.put("empty", ItemList.Tool_Lighter_Invar_Empty.get(1L));
+            lighterStates.put("used", ItemList.Tool_Lighter_Invar_Used.get(1L));
+            lighterStates.put("full", ItemList.Tool_Lighter_Invar_Full.get(1L));
+        }
+    }
+
+    private void handleLighterFuel(ItemStack lighterItemStack, boolean isPlatinum) {
+        long newFuelAmount = isFullLighter(lighterItemStack) ? (isPlatinum ? 999L : 99L)
+            : getLighterFuel(lighterItemStack) - 1;
+        GT_Utility.sendSoundToPlayers(
+            Minecraft.getMinecraft().theWorld,
+            SoundResource.FIRE_IGNITE,
+            1.0F,
+            1.0F,
+            (int) Minecraft.getMinecraft().thePlayer.posX,
+            (int) Minecraft.getMinecraft().thePlayer.posY,
+            (int) Minecraft.getMinecraft().thePlayer.posZ);
+        GT_Utility.ItemNBT.setLighterFuel(lighterItemStack, newFuelAmount);
+        updateLighterTexture(lighterItemStack, newFuelAmount);
+    }
+
+    private boolean isFullLighter(ItemStack lighterItemStack) {
+        return lighterItemStack.getDisplayName()
+            .contains("Lighter (Full)");
+    }
+
+    private long getLighterFuel(ItemStack lighterItemStack) {
+        return GT_Utility.ItemNBT.getLighterFuel(lighterItemStack);
+    }
+
+    private void updateLighterTexture(ItemStack lighterItemStack, long newFuelAmount) {
+        if (newFuelAmount <= 0) {
+            useUp(lighterItemStack);
+        } else {
+            prepare(lighterItemStack, newFuelAmount);
+        }
+    }
+
+    private void setCigaretteLit(ItemStack itemStackIn, World worldIn) {
+        NBTTagCompound tagCompound = itemStackIn.getTagCompound();
+        if (tagCompound == null) tagCompound = new NBTTagCompound();
+        if (!tagCompound.getBoolean("lit")) {
+            tagCompound.setBoolean("lit", true);
+            itemStackIn.setTagCompound(tagCompound);
+        }
+        tagCompound.setLong("lastUseTime", worldIn.getTotalWorldTime());
+        itemStackIn.setTagCompound(tagCompound);
+    }
+
+    private void spawnSmokeParticles(EntityPlayer player, World worldIn) {
+        Map<String, Double> smokePos = getPositionInfrontofPlayerFace(player);
+        Map<String, Double> smokeOffsets = getOffsetInfrontofPlayerFace(player);
+        for (int i = 0; i < 5; i++) {
+            Minecraft.getMinecraft().effectRenderer.addEffect(
+                new EntitySteamFX(
+                    worldIn,
+                    smokePos.get("x"),
+                    smokePos.get("y"),
+                    smokePos.get("z"),
+                    smokeOffsets.get("x") / 10,
+                    smokeOffsets.get("y") / 10,
+                    smokeOffsets.get("z") / 10));
+        }
+    }
+
+    private void applyCoolStatusEffect(EntityPlayer player) {
+        player.removePotionEffect(ModPotions.PotionIDConverter.get("coolPotion"));
+        player.addPotionEffect(
+            new PotionEffect(ModPotions.PotionIDConverter.get("coolPotion"), _coolStatusEffectDuration, 0));
+    }
+
+    private ItemStack handleCigaretteDamage(ItemStack itemStackIn, EntityPlayer player) {
+        if (itemStackIn.getItemDamage() == itemStackIn.getMaxDamage()) {
+            ItemStack newStack = new ItemStack(_cigaretteButt, 1);
+            player.inventory.setInventorySlotContents(player.inventory.currentItem, newStack);
+            player.inventory.markDirty();
+            return newStack;
+        }
+        return itemStackIn;
+    }
 
     private Map<String, Double> getOffsetInfrontofPlayerFace(EntityPlayer player) {
         // Calculate the position in front of the player's face
@@ -276,6 +288,7 @@ public class CigaretteBase extends Item {
         data.put("z", offsetZ);
         return data;
     }
+
     private Map<String, Double> getPositionInfrontofPlayerFace(EntityPlayer player) {
         // Calculate the position in front of the player's face
         double playerPosX = player.posX;
